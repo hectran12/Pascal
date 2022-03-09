45f29efa8201cad2190f79812e655811beec6ef5
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2022-03-09 09:30:03 +0700

    Chương trình đầu tiên.
    In ra chữ "hello world" với 5 dòng code cơ bản.

---
+++ hello_world.pas
@@ -0,0 +1,5 @@
+program chuong_trinh_dau; (*dat ten cho chuong trinh*)
+begin (*bat dau chuong trinh*)
+     writeln('hello world'); (*in ra hello world*)
+     readln; (*nguong lai de xem ket qua*)
+end.
