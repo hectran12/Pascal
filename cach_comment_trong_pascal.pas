9af89971f87feb0af7979a95a44d02dc16364e39
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2022-03-09 09:46:24 +0700

    Cách comment trong pascal.
    Tập luyện khả năng comment, và comment thường xuyên khi code, giúp bạn cải thiện nhiều về thời gian code và làm việc chung đấy :D.

---
+++ cach_comment_trong_pascal.pas
@@ -0,0 +1,21 @@
+program comment_in_pascal;
+begin
+   (*Cach comment tren pascal, va day la 1 comment :D*)
+   writeln('Comment ben tren , se khong duoc in ra man hinh');
+
+   (*
+     Day la cach comment thu 2
+     Comment nhieu dong
+   *)
+   writeln('Comment nhieu dong , se khong duoc hien ra');
+
+   { (*Comment dang khac, nhung dep hon :>*) }
+   writeln('Comment ben dang khac , se khong duoc xuat hien khi chay');
+
+   (*Comment de {lam gi}, comment giup ban thuan loi trong viec code va chinh sua code
+      Cung nhu tang hieu qua lam viec chung!*)
+   writeln('Comment de lam gi?');
+
+   (*Ngung chuong trinh, de nhin*)
+   readln;
+end.
