9c43fdaa5921752df85320ff2b39a8065d9969b7
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2022-06-08 06:10:08 +0700

    Create format.pas

---
+++ format.pas
@@ -0,0 +1,11 @@
+begin
+    (*Chuỗi*)
+    writeln('Hoa':3,'Dep trai':9);
+    {
+        GIẢI THÍCH NHƯ SAU: Format cho 'Hoa' giới hạn 3 kí tự và 'Đẹp trai' giới hạn 9 kí tự trong đó nó có 8 thì dư 1
+        Output: Hoa Dep trai
+    }
+
+    (*Kiểu dữ liệu khác*)
+    writeln('Số Pi la: ',Pi:4:2);
+end.
