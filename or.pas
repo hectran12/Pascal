8631e09aaf22eac5ecf5e5b6066a7036e80cb1fb
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2022-06-12 09:25:42 +0700

    Create or.pas

---
+++ or.pas
@@ -0,0 +1,13 @@
+program demo2;
+const
+    isNumber_One   = True;
+    isNumber_Two   = False;
+    isNumber_Three = True;
+begin
+
+    if (isNumber_One or isNumber_Two) then
+        write('Số 1 hoặc số 2 là True')
+    else
+        write('Không có số nào là True');
+    
+end.
