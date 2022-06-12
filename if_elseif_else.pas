352a64626cb1bb49ff39ced7f8c1616a5dce47ad
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2022-06-12 11:40:10 +0700

    Create if_elseif_else.pas

---
+++ if_elseif_else.pas
@@ -0,0 +1,16 @@
+program if_elseif_else;
+const
+    a = 30;
+    b = 20;
+begin
+    
+    if (a > b) then begin
+        write('A lớn hơn B');
+    end
+    else if (a = b) then begin
+        write('A bằng B');
+    end
+    else
+        write('A nhỏ hơn B')
+
+end.
