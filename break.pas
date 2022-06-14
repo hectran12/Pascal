fa669dbd2f4fa0199f013c009c4db0be4a0bd055
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2022-06-14 15:32:31 +0700

    Create break.pas

---
+++ break.pas
@@ -0,0 +1,35 @@
+program repeat_until;
+
+var
+    i : integer;
+
+begin
+
+    (*while*)
+    i := 0;
+
+    while i < 20 do begin
+        if i = 15 then begin
+            writeln('I = 15 và đã ngưng ( while )');
+            break;
+
+        end;
+        i := i + 1;
+    end;
+
+    (*for*)
+    for i:=0 to 20 do begin
+        if i = 15 then begin
+            writeln('I = 15 và đã ngưng ( for )');
+        end;
+    end;
+
+    (*repeat until*)
+    i := 0;
+    repeat
+        if i = 15 then begin
+            writeln('I = 15 và đã ngưng ( repeat - until )');
+        end;
+        i := i + 1;
+    until i = 20;
+end.
