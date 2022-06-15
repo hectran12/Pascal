b059335e438165ae36ec3945cdf74638fcc26453
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2022-06-15 11:38:25 +0700

    Create procedure2.pas

---
+++ procedure2.pas
@@ -0,0 +1,30 @@
+program global_procedure;
+
+var
+    tenz , frz : string;
+
+procedure printInfo();
+var
+    ten , fr : string;
+begin
+
+    writeln(tenz);
+    writeln(frz);
+
+    ten := 'Trong Hoa';
+    fr := '100';
+    writeln(ten);
+    writeln(fr);
+
+
+end;
+
+
+begin
+
+    tenz := 'Nhan hoa';
+    frz := '2000';
+
+    printInfo();
+
+end.
