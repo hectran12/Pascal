a74fd260b56e8ae1c20d1f9e260245a4cf4ac7a1
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2022-06-21 22:05:36 +0700

    Create baitap.pas

---
+++ baitap.pas
@@ -0,0 +1,51 @@
+program baitap;
+
+type
+    number = record
+                daidien : string;
+                solanxh : integer;
+            end;
+var
+    i,a,z: integer;
+    input : string;
+    numbers : array[0..100] of number;
+    sess : string;
+    check : boolean;
+begin
+
+    write('Nhập: ');
+    readln(input);
+    
+    for i:=1 to length(input) do begin
+        for a:=1 to length(sess) do begin
+            if sess[a] = input[i] then begin
+                check := False;
+                break;
+            end
+            else
+                check := True;
+        end;
+
+        if check = True then begin
+            sess := sess + input[i];
+        end
+        else if length(sess) = 0 then sess := sess + input[i];
+    end;
+    
+
+    for a:=1 to length(sess) do begin
+        z := 0;
+        for i:=1 to length(input) do begin
+            if input[i] = sess[a] then begin
+                z := z + 1;
+            end;
+        end;
+        numbers[a-1].daidien := sess[a];
+        numbers[a-1].solanxh := z;
+    end;
+
+
+    for a:=0 to length(sess)-1 do begin
+        writeln('Number: ',numbers[a].daidien,' , số lần xuất hiện: ',numbers[a].solanxh);
+    end;
+end.
