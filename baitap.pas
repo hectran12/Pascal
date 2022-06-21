29b1a92fdaef66846c58f5104cd94af1019a87c1
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2022-06-21 23:44:14 +0700

    Update baitap.pas

--- baitap.pas
+++ baitap.pas
@@ -11,11 +11,18 @@ var
     numbers : array[0..100] of number;
     sess : string;
     check : boolean;
-begin
 
-    write('Nhập: ');
-    readln(input);
-    
+procedure loaibotrong();
+begin
+    for i:=1 to length(input) do begin
+        if input[i] <> ' ' then sess := sess + input[i];
+    end;
+    input := sess;
+    sess := '';
+end;
+
+procedure lockitu();
+begin
     for i:=1 to length(input) do begin
         for a:=1 to length(sess) do begin
             if sess[a] = input[i] then begin
@@ -31,8 +38,10 @@ begin
         end
         else if length(sess) = 0 then sess := sess + input[i];
     end;
-    
+end;
 
+procedure demso();
+begin
     for a:=1 to length(sess) do begin
         z := 0;
         for i:=1 to length(input) do begin
@@ -43,8 +52,14 @@ begin
         numbers[a-1].daidien := sess[a];
         numbers[a-1].solanxh := z;
     end;
+end;
+begin
 
-
+    write('Nhập: ');
+    readln(input);
+    loaibotrong();
+    lockitu();
+    demso();
     for a:=0 to length(sess)-1 do begin
         writeln('Number: ',numbers[a].daidien,' , số lần xuất hiện: ',numbers[a].solanxh);
     end;
